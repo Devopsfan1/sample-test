5c8011b8ae6ee3f911c78f3f0d1035f8543f626d
Pooja Naik <pooja.naik2@ibm.com>
2023-12-21 09:54:03 +0530

    added new files

---
+++ src/main/java/testdummy/demo/TestDummyApplication.java
@@ -0,0 +1,13 @@
+package testdummy.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestDummyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestDummyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/testdummy/demo/TestDummyApplicationTests.java
@@ -0,0 +1,13 @@
+package testdummy.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestDummyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
